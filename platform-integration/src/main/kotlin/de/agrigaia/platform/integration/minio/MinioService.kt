62cf5c447adae35b5ffce471f192468dd30c4389
Enis Belli <Enis.Belli@LMIS.de>
2022-11-25 13:42:05 +0100

    Working Minio client

--- platform-integration/src/main/kotlin/de/agrigaia/platform/integration/minio/MinioService.kt
+++ platform-integration/src/main/kotlin/de/agrigaia/platform/integration/minio/MinioService.kt
@@ -1,22 +1,27 @@
 package de.agrigaia.platform.integration.minio
+
 import io.minio.MinioClient
+import io.minio.credentials.Jwt
+import io.minio.credentials.WebIdentityProvider
 import io.minio.messages.Bucket
 import org.springframework.stereotype.Service
 
 
 @Service
-class MinioService constructor() {
-
-    // TODO move endpoint etc. into configuration
-    private var minioClient = MinioClient.builder()
-        .endpoint("https://minio-test-api.platform.agri-gaia.com")
-        .credentials("7SBIOO6XCSUYV81732ED", "eR3hBk8JNX87m1ieRog4zB+5GcXi1Ixmxd5pc72+")
-        //.credentials("api-test-user", "test1234")
-        .build()
-
+class MinioService {
     fun listBuckets(jwt: String): MutableList<Bucket>? {
-        println(" listBuckets ");
-        var buckets = minioClient.listBuckets();
+        val minioClient = MinioClient.builder()
+            .credentialsProvider(
+                WebIdentityProvider(
+                    { Jwt(jwt, 8600) },
+                    "https://minio-test-api.platform.agri-gaia.com",
+                    null, null, null, null, null
+                )
+            )
+            .endpoint("https://minio-test-api.platform.agri-gaia.com")
+            .build()
+
+        val buckets = minioClient.listBuckets()
         return buckets;
     }
 }
