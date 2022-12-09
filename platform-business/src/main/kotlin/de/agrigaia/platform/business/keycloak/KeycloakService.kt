0b09c599871703cc3a55feabdaa8a6f77480e5e0
Enis Belli <Enis.Belli@LMIS.de>
2022-12-09 11:40:19 +0100

    Small cleanup

--- platform-business/src/main/kotlin/de/agrigaia/platform/business/keycloak/KeycloakService.kt
+++ platform-business/src/main/kotlin/de/agrigaia/platform/business/keycloak/KeycloakService.kt
@@ -13,23 +13,7 @@ class KeycloakService(
 ) {
     fun getKeycloakUsers(): List<Member> {
         val users = this.keycloakConnectorService.getUsers()
-        for (user in users){
-            println(user.firstName)
-        }
-        return listOf(
-            Member("Alejandro Lopez", "Bosch", "alejandro.lopez2@de.bosch.com", CoopSpaceRole.GUEST, "alopez"),
-            Member("Julian Ende", "LMIS", "julian.ende@lmis.de", CoopSpaceRole.USER, "jende"),
-            Member("Enis Belli", "LMIS", "enis.belli@lmis.de", CoopSpaceRole.USER, "ebelli"),
-            Member(
-                "Katharina Beckwermert",
-                "LMIS",
-                "katharina.beckwermert@lmis.de",
-                CoopSpaceRole.USER,
-                "kbeckwermert"
-            ),
-            Member("Henning Wuebben", "LMIS", "henning.wuebben@lmis.de", CoopSpaceRole.USER, "hwuebben"),
-            Member("Christoph Manß", "DFKI", "christoph.manss@dfki.de", CoopSpaceRole.USER, "cmanss")
-        )
+        return users.map { Member(it.firstName + it.lastName, "company", it.email, CoopSpaceRole.USER, it.username) }
     }
 
     fun findKeycloakUserByMail(mail: String): Member {
