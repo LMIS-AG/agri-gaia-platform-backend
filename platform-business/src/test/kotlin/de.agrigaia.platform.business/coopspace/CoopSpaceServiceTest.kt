e342d3fa82c4700ec53c25cf0866c5e0900fddb6
Marcel Ruland <marcel.ruland@lmis.de>
2023-02-22 15:23:17 +0100

    Add tests for changeUserRoleInDatabase

--- platform-business/src/test/kotlin/de.agrigaia.platform.business/coopspace/CoopSpaceServiceTest.kt
+++ platform-business/src/test/kotlin/de.agrigaia.platform.business/coopspace/CoopSpaceServiceTest.kt
@@ -1,8 +1,11 @@
 package de.agrigaia.platform.business.coopspace
 
+import de.agrigaia.platform.business.errors.BusinessException
+import de.agrigaia.platform.business.errors.ErrorType
 import de.agrigaia.platform.integration.keycloak.KeycloakConnectorService
 import de.agrigaia.platform.integration.minio.MinioService
 import de.agrigaia.platform.model.coopspace.CoopSpace
+import de.agrigaia.platform.model.coopspace.CoopSpaceRole
 import de.agrigaia.platform.model.coopspace.Member
 import de.agrigaia.platform.persistence.repository.CoopSpaceRepository
 import de.agrigaia.platform.persistence.repository.MemberRepository
@@ -10,6 +13,7 @@ import io.minio.messages.Bucket
 import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.util.*
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -129,6 +133,77 @@ class CoopSpaceServiceTest {
     }
 
     @Test
+    fun `Test changeUserRoleInDatabase`() {
+        val coopSpacesProperties: CoopSpacesProperties = mockk()
+        val coopSpaceRepository: CoopSpaceRepository = mockk()
+        val memberRepository: MemberRepository = mockk()
+        val minioService: MinioService = mockk()
+        val keycloakConnectorService: KeycloakConnectorService = mockk()
+        val coopSpaceService = CoopSpaceService(
+            coopSpacesProperties = coopSpacesProperties,
+            coopSpaceRepository = coopSpaceRepository,
+            memberRepository = memberRepository,
+            minioService = minioService,
+            keycloakConnectorService = keycloakConnectorService,
+        )
+        every { memberRepository.save(any()) } returns Member()
+        val matchingUserName = "Some Holy User Name"
+
+        // Throw BusinessException(ErrorType.NOT_FOUND) if Member with matching `username` is not in CoopSpace.
+        val `member lessCoopSpace` = CoopSpace(members = listOf())
+        val passedMember = Member(
+            username = matchingUserName,
+            role = CoopSpaceRole.USER,
+        )
+        passedMember.id = 43
+        val exception =
+            assertThrows<BusinessException>("Should throw `BusinessException` if no matching user is found.") {
+                coopSpaceService.changeUserRoleInDatabase(
+                    passedMember,
+                    `member lessCoopSpace`
+                )
+            }
+        assertEquals(
+            exception.errorCode,
+            ErrorType.NOT_FOUND,
+            "Should have thrown BusinessException of type `NOT_FOUND`."
+        )
+
+        // Call memberRepository.save(), passing member object with field values from Member in CoopSpace, except `role` and `id`, which should have the value of the passed Member parameter.
+        // Setup CoopSpace member.
+        val coopSpaceMember = Member(
+            name = "Some Name",
+            company = "someCompany",
+            email = "someEmail",
+            role = CoopSpaceRole.GUEST,
+            username = matchingUserName
+        )
+        coopSpaceMember.id = 42
+        val dummyCoopSpace = CoopSpace(members = listOf(coopSpaceMember))
+
+        // Set up saved member.
+        val expectedSavedMember = Member(
+            name = coopSpaceMember.name,
+            company = coopSpaceMember.company,
+            email = coopSpaceMember.email,
+            role = passedMember.role,
+            username = matchingUserName
+        )
+        expectedSavedMember.id = passedMember.id
+        coopSpaceService.changeUserRoleInDatabase(passedMember, dummyCoopSpace)
+        verify {
+            memberRepository.save(match { actualSavedMember ->
+                actualSavedMember.name == expectedSavedMember.name &&
+                        actualSavedMember.company == expectedSavedMember.company &&
+                        actualSavedMember.email == expectedSavedMember.email &&
+                        actualSavedMember.role == expectedSavedMember.role &&
+                        actualSavedMember.username == expectedSavedMember.username &&
+                        actualSavedMember.id == expectedSavedMember.id
+            })
+        }
+    }
+
+    @Test
     fun `Test hasAccessToCoopSpace`() {
         val coopSpacesProperties: CoopSpacesProperties = mockk()
         val coopSpaceRepository: CoopSpaceRepository = mockk()
@@ -145,12 +220,13 @@ class CoopSpaceServiceTest {
 
         val c: CoopSpace = mockk()
         every { c.members } returns listOf()
+        assertFalse(coopSpaceService.hasAccessToCoopSpace("", c), "Should return false when given empty username.")
         assertFalse(
             coopSpaceService.hasAccessToCoopSpace("Antonia", c), "Cannot have access to coopSpace with no members."
         )
-        assertFalse(coopSpaceService.hasAccessToCoopSpace("", c), "Should return false when given empty username.")
 
         every { c.members } returns listOf(Member(username = "Antonia"), Member(username = "Norbert"))
+        assertFalse(coopSpaceService.hasAccessToCoopSpace("", c), "Should return false when given empty username.")
         assertTrue(
             coopSpaceService.hasAccessToCoopSpace("Antonia", c),
             "Should have access to coopSpace containing member with matching username."
