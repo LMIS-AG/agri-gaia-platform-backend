5fb19705c3603dc58a7f49c949aa40dbaf49085b
julian-marco <julian.ende@lmis.de>
2023-03-01 15:18:51 +0100

    put endpoint back in that was deleted accidentally

--- platform-api/src/main/kotlin/de/agrigaia/platform/api/assets/AssetsController.kt
+++ platform-api/src/main/kotlin/de/agrigaia/platform/api/assets/AssetsController.kt
@@ -21,6 +21,15 @@ class AssetsController @Autowired constructor(
     private val minioService: MinioService
 ) : HasLogger, BaseController() {
 
+    @DeleteMapping("delete/{bucket}/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    fun deleteAsset(@PathVariable bucket: String, @PathVariable name: String) {
+        val jwtAuthenticationToken = SecurityContextHolder.getContext().authentication as JwtAuthenticationToken
+        val jwt = jwtAuthenticationToken.token.tokenValue
+
+        this.minioService.deleteAsset(jwt, bucket, name)
+    }
+
     @PostMapping("publish/{bucket}/{name}")
     @ResponseStatus(HttpStatus.CREATED)
     fun publishAsset(@PathVariable bucket: String, @PathVariable name: String) {
