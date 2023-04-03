fc331863721872dfef7d543c853e6bb4a9d8e099
Marcel Ruland <info@marcelruland.com>
2023-04-03 11:28:12 +0200

    Add MethodSecurityConfig

---
+++ platform-api/src/main/kotlin/de/agrigaia/platform/api/config/MethodSecurityConfig.kt
@@ -0,0 +1,8 @@
+package de.agrigaia.platform.api.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+
+@Configuration
+@EnableMethodSecurity
+open class MethodSecurityConfig
