403abae66f5a57720de1d4c56a722431ab9f82cb
Marcel Ruland <info@marcelruland.com>
2023-04-04 14:33:43 +0200

    Delete obsolete config (it was nice while it lasted)

--- platform-api/src/main/kotlin/de/agrigaia/platform/api/config/MethodSecurityConfig.kt
+++
@@ -1,8 +0,0 @@
-package de.agrigaia.platform.api.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-
-@Configuration
-@EnableMethodSecurity
-open class MethodSecurityConfig
