988aa81c24accc958056b41fa04725cdbb9d4d1b
Marcel Ruland <info@marcelruland.com>
2023-04-11 12:21:54 +0200

    Get rid of example stuff

--- platform-api/src/main/kotlin/de/agrigaia/platform/api/example/ExampleDto.kt
+++
@@ -1,9 +0,0 @@
-package de.agrigaia.platform.api.example
-
-import de.agrigaia.platform.api.BaseDto
-
-data class ExampleDto(
-    var id: Long?,
-    var lineOne: String?,
-    var lineTwo: String?,
-) : BaseDto()
