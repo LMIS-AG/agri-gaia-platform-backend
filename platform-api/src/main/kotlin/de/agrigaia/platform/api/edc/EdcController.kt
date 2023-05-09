faed0567e9389a51557c5da0be96a28daa0c4760
Marcel Ruland <info@marcelruland.com>
2023-05-09 10:48:03 +0200

    Add endpoint skeletons

--- platform-api/src/main/kotlin/de/agrigaia/platform/api/edc/EdcController.kt
+++ platform-api/src/main/kotlin/de/agrigaia/platform/api/edc/EdcController.kt
@@ -81,4 +81,55 @@ class EdcController @Autowired constructor(
         this.edcConnectorService.unpublishAsset(assetId!!, policyId!!, contractId!!)
 
     }
+
+
+    /**
+     * Returns a list of policy names from a Minio bucket.
+     *
+     * @param bucketName name of the MinIO bucket
+     * @return list of names of the policies in MinIO bucket `bucketName`
+     */
+    @GetMapping("policies/{bucketName}")
+    fun getPolicyNames(@PathVariable bucketName: String) {
+        TODO("Not yet implemented")
+    }
+
+
+    /**
+     * Save a policy to a MinioBucket.
+     *
+     * @param bucketName name of the MinIO bucket
+     * @param policyName name of the policy
+     * @return TODO
+     */
+    @PostMapping("policies/{bucketName}/{policyName}")
+    fun savePolicy(@PathVariable policyName: String, @RequestBody policyJson: String) {
+        TODO("Not yet implemented")
+    }
+
+
+    /**
+     * Delete a policy from a MinIO bucket.
+     *
+     * @param bucketName name of the MinIO bucket
+     * @param policyName name of the policy
+     * @return TODO
+     */
+    @DeleteMapping("policies/{bucketName}/{policyName}")
+    fun deletePolicy(@PathVariable bucketName: String, @PathVariable policyName: String) {
+        TODO("Not yet implemented")
+    }
+
+
+    /**
+     * Get a policy from a MinIO bucket.
+     *
+     * @param bucketName name of the MinIO bucket
+     * @param policyName name of the policy
+     * @return TODO
+     */
+    @GetMapping("policies/{bucketName}/{policyName}")
+    fun getPolicy(@PathVariable bucketName: String, @PathVariable policyName: String) {
+        TODO("Not yet implemented")
+    }
 }
