d85403c8dceb010be536e404157f67b23d609cf2
Marcel Ruland <info@marcelruland.com>
2023-05-22 12:03:58 +0200

    Add rawJson field to DTO

--- platform-model/src/main/kotlin/de/agrigaia/platform/model/edc/PolicyDto.kt
+++ platform-model/src/main/kotlin/de/agrigaia/platform/model/edc/PolicyDto.kt
@@ -2,8 +2,9 @@ package de.agrigaia.platform.model.edc
 
 // TODO: This should go into api, but I cba.
 data class PolicyDto(
-    val name: String,
-    val policyType: PolicyType,
+    val name: String?,
+    val policyType: PolicyType?,
+    val rawJson: String?,
 )
 
 enum class PolicyType {
