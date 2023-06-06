bd643183587e049eb56e04506d7bc934e295e61b
Marcel Ruland <info@marcelruland.com>
2023-06-06 12:06:26 +0200

    Make DTO fields nullable

--- platform-api/src/main/kotlin/de/agrigaia/platform/api/coopspace/AssetDto.kt
+++ platform-api/src/main/kotlin/de/agrigaia/platform/api/coopspace/AssetDto.kt
@@ -3,11 +3,11 @@ package de.agrigaia.platform.api.coopspace
 import de.agrigaia.platform.api.BaseDto
 
 data class AssetDto(
-    val name: String,
-    val date: String,
-    val uploadDate: String,
-    val size: String,
-    val labeling: String,
-    val bucket: String,
-    val isPublished: Boolean
+    val name: String?,
+    val date: String?,
+    val uploadDate: String?,
+    val size: String?,
+    val labeling: String?,
+    val bucket: String?,
+    val isPublished: Boolean?
 ) : BaseDto()
